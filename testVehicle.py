d45edd8aa5bc80e8bbb6a92f4a3d27f022c7eed1
eoinok <55285386+eoinok@users.noreply.github.com>
2022-02-16 23:56:20 +0000

    Update testVehicle.py

--- testVehicle.py
+++ testVehicle.py
@@ -2,7 +2,9 @@ from Vehicle import Vehicle
 myVehicle = Vehicle("00D1122","Ford","Mondeo")
 myVehicle.set_miles(10000)
 print("Your Vehicle is a ")
+print("adding another line in the local repo")
 print("added this extra line in the local repo")
 print(myVehicle.get_make_model())
 print("added this line in the remote repo")
+
 print("with " + str(myVehicle.get_miles()) + " miles on the clock")
