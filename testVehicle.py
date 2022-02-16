bc8e665ddf289a3eb9e203bf4ca74ace3bc83c9e
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-02-16 23:50:57 +0000

    local change to testVehicle

--- testVehicle.py
+++ testVehicle.py
@@ -2,5 +2,6 @@ from Vehicle import Vehicle
 myVehicle = Vehicle("00D1122","Ford","Mondeo")
 myVehicle.set_miles(10000)
 print("Your Vehicle is a ")
+print("added this extra line in the local repo")
 print(myVehicle.get_make_model())
 print("with " + str(myVehicle.get_miles()) + " miles on the clock")
